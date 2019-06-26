aba52b8f7db2a729ee37f8922d18f3ab1ff827a9
Dmitriy <this.demetreus@gmail.com>
2019-06-26 03:56:12 +0300

    wip

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -5,3 +5,9 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+const PATHS = {
+    src: path.join(__dirname, '../src'),
+    dist: path.join(__dirname, '../dist'),
+    assets: 'assets'
+};
+
